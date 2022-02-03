420a3003558347dd4cbf7e7340e28fbaf20ea907
aro4x4 <vlad2804@yahoo.com>
2022-02-03 09:53:43 +0200

    computer random

---
+++ game.js
@@ -0,0 +1,14 @@
+
+//computer input//
+
+const choose = [ "rock", "paper", "scissors" ];
+const randIndex = Math.floor(Math.random() * choose.length);
+computerSelection = choose[randIndex];
+console.log(computerSelection);
+
+//user input//
+
+
+userSelection = nextLine();
+
+//play round//
