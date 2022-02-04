0ce209696672070ad13b0db6d5695595b238cf34
aro4x4 <vlad2804@yahoo.com>
2022-02-04 11:00:15 +0200

    play round

--- game.js
+++ game.js
@@ -1,14 +1,34 @@
+//player input
+console.log("ROCK - PAPER - SCISSORS");
+const string = prompt("ROCK-PAPER-SCISSORS Please enter your choice: ");
+const playerSelection = string.toLowerCase();
+console.log("Player choice: " + playerSelection);
 
-//computer input//
+//computer input
 
-const choose = [ "rock", "paper", "scissors" ];
-const randIndex = Math.floor(Math.random() * choose.length);
-computerSelection = choose[randIndex];
-console.log(computerSelection);
+function computerPlay() {
+    const choice = ["rock", "paper", "scissors"];
+    const randIndex = Math.floor(Math.random() * choice.length);
+    const randomSelection = choice[randIndex];
+    return(randomSelection);
+}
 
-//user input//
+//play round
 
+const computerSelection = computerPlay();
+console.log("Computer choice: " + computerSelection);
 
-userSelection = nextLine();
+function playRound() {    
+    const result = playerSelection + computerSelection;
+    if (playerSelection == computerSelection) {
+    console.log("It's a tie.");
+}
 
-//play round//
+else if (result == "rockscissors" || result == "paperrock" || result == "scissorspaper") {
+    console.log("You win! " + playerSelection + " beats " + computerSelection);
+}
+else
+    console.log("You loose. " + computerSelection + " beats " + playerSelection);
+
+}
+playRound();
