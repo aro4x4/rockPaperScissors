6cd77ef45f65c5c8b41d722064a31f06fa686721
aro4x4 <vlad2804@yahoo.com>
2022-03-31 16:11:24 +0300

    switched input to buttons

--- game.js
+++ game.js
@@ -2,11 +2,10 @@
 console.log("       |--ROCK - PAPER - SCISSORS--");
 
 //player input
-function playerInput() {
-    const string = prompt("ROCK-PAPER-SCISSORS Please enter your choice: ");
-    return string.toLowerCase();
-}
-var playerPoints = 0;
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.innerHTML , computerPlay())));
+
+var int_playerPoints = 0;
 
 //computer input
 function computerPlay() {
@@ -15,13 +14,11 @@ function computerPlay() {
     const randomSelection = choice[randIndex];
     return(randomSelection);
 }
-var computerPoints = 0;
+var int_computerPoints = 0;
 
 //play round
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerInput();
     console.log("       |   Player choice: " + playerSelection);
-    computerSelection = computerPlay(); 
     console.log("       |   Computer choice: " + computerSelection);
     const result = playerSelection + computerSelection;
     if (playerSelection == computerSelection) {
@@ -30,38 +27,38 @@ function playRound(playerSelection, computerSelection) {
 
 else if (result == "rockscissors" || result == "paperrock" || result == "scissorspaper") {
     console.log("       | You win! " + playerSelection + " beats " + computerSelection);
-    playerPoints = playerPoints + 1;
+    int_playerPoints = int_playerPoints + 1;
 }
 else {
     console.log("       | You lose. " + computerSelection + " beats " + playerSelection);
-    computerPoints = computerPoints + 1;
+    int_computerPoints = int_computerPoints + 1;
 }
-    console.log("       |Player points:", playerPoints, "Computer points:", computerPoints );
+    console.log("       |Player points:", int_playerPoints, "Computer points:", int_computerPoints );
 }
 
 //five round game
-function game() {
-console.log("       |   ROUND 1 / 5");
-playRound();
-console.log("       |   ROUND 2 / 5");
-playRound();
-console.log("       |   ROUND 3 / 5");
-playRound();
-console.log("       |   ROUND 4 / 5");
-playRound();
-console.log("       |   ROUND 5 / 5");
-playRound();
-function winner () {
-    if (playerPoints == computerPoints) {
-        console.log("       |--IT'S A TIE--");
-    }
-    else if (playerPoints > computerPoints) {
-        console.log("       |--Congratulations, YOU WON!--");
-    }
-    else {
-        console.log("       |--YOU LOST--");
-    }
-}
-winner()
-}
-game()
+//function game() {
+//console.log("       |   ROUND 1 / 5");
+//playRound();
+//console.log("       |   ROUND 2 / 5");
+//playRound();
+//console.log("       |   ROUND 3 / 5");
+//playRound();
+//console.log("       |   ROUND 4 / 5");
+//playRound();
+//console.log("       |   ROUND 5 / 5");
+//playRound();
+//function winner () {
+//    if (int_playerPoints == int_computerPoints) {
+//        console.log("       |--IT'S A TIE--");
+//    }
+//    else if (int_playerPoints > int_computerPoints) {
+//        console.log("       |--Congratulations, YOU WON!--");
+//    }
+//    else {
+//        console.log("       |--YOU LOST--");
+//    }
+//}
+//winner()
+//}
+//game()
